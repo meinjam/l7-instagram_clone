036718dc56976c457c5186f33111dbcbca91ab04
meinjam <injam.bd.jsr@gmail.com>
2020-04-11 16:47:29 +0600

    user profile page design done

---
+++ resources/views/home.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-3 p-5">
+            <img src="img/profile.jpg" height="160" alt="profile image" class="rounded-circle">
+        </div>
+        <div class="col-md-9 pt-5">
+            <div><h2>queentzuyu</h2></div>
+            <div class="d-flex">
+                <div class="pr-4"><strong>1,752</strong> posts</div>
+                <div class="pr-4"><strong>867k</strong> followers</div>
+                <div class="pr-4"><strong>2</strong> following</div>
+            </div>
+            <div class="pt-4"><h5>TWICE TZUYU (주자유)</h4></div>
+            <div>fp. est: 06092015 💛</div>
+            <div><a href="">youtu.be/kOHB85vDuow</a></div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-4">
+            <img src="https://instagram.fdac10-1.fna.fbcdn.net/v/t51.2885-15/e35/c0.179.1440.1440a/s320x320/92446986_3790941537615006_7396469934900999096_n.jpg?_nc_ht=instagram.fdac10-1.fna.fbcdn.net&_nc_cat=106&_nc_ohc=JQtpaGCHs2QAX-1_-em&oh=6cfed7984be3a39677139cdf57e502f3&oe=5EBC36D3" alt="">
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.fdac10-1.fna.fbcdn.net/v/t51.2885-15/e35/s320x320/92300926_2806066886287160_2376820039568614972_n.jpg?_nc_ht=instagram.fdac10-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=DAT81IkbM0YAX8qlNLW&oh=8d4c32df2174c77524e3167c626fcacb&oe=5EBAC3A0" alt="">
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.fdac10-1.fna.fbcdn.net/v/t51.2885-15/e35/c0.180.1440.1440a/s320x320/82165713_113400813382669_4540044365122850125_n.jpg?_nc_ht=instagram.fdac10-1.fna.fbcdn.net&_nc_cat=101&_nc_ohc=zzy-FLuA26EAX94bR-H&oh=a32b01f090075c3e7bf78448290ddab3&oe=5EBC24F0" alt="">
+        </div>
+    </div>
+</div>
+@endsection
